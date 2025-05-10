eda60d68155cf8dbe890f42f981cfa1ffa4a3cb2
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-05-10 21:37:34 +0300

    feat: Add NetworkUtils and PermissionManager for Android

---
+++ app/src/main/java/com/zarea/instabugtask/data/source/remote/NetworkUtils.kt
@@ -0,0 +1,21 @@
+package com.zarea.instabugtask.data.source.remote
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+class NetworkUtils(private val context: Context) {
+
+    fun isNetworkAvailable(): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        val activeNetwork = connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+        
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    }
+}
